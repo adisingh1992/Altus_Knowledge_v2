9ec2097b21fa0aa797d5eef3b88b3151904cd37e
Aditya Singh <aditya@cooey.co.in>
2019-12-09 20:35:00 +0530

    URL Update

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-	rootUrl: 'https://altusknowledge.firebaseapp.com/api/v1',
+	rootUrl: 'https://www.altusknowledge.co.in/api/v1',
 	payUBizURL: 'https://secure.payu.in/_payment',
 	production: true
 };
--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	rootUrl: 'https://altusknowledge.firebaseapp.com/api/v1',
+	rootUrl: 'https://www.altusknowledge.firebaseapp.com/api/v1',
 	// rootUrl: 'http://localhost:5000/api/v1',
 	payUBizURL: "https://test.payu.in/_payment",
 	production: false
